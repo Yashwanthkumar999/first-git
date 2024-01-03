4d23d485a2c31e1d33c022555c0d4408218e167d
yashwanth kumar <yashwanthg_25@outlook.com>
2024-01-03 22:54:23 +0530

    java1

---
+++ info.java
@@ -0,0 +1,20 @@
+
+    import java.util.Scanner;
+/**
+ * info
+ */
+public class info {
+
+    public static void main(String[] args) {
+        java.util.Scanner enter=new Scanner(System.in);
+        int x;
+        String name;
+        System.out.println("enter you name:");
+        name=enter.nextLine();
+        System.out.println("enter your lucky number");
+        x=enter.nextInt();
+        System.out.println("HI"+" "+name);
+        System.out.println("based on your lucky number:"+"#"+x+" "+"YOU ARE COOL!");
+    }
+}
+
